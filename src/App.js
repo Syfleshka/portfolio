48affda5a5e16dce1850d14016bc23601d167ddc
Syfle <syfle.denisov@gmail.com>
2022-03-28 18:04:54 +0300

    Add theme switch function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.scss';
 import Logo from "./Logo/Logo";
 import Menu from "./Menu/Menu";
+import {useState} from "react";
 
 function App() {
-  return (
-      <header className="header dark">
-          <Logo />
-          <Menu />
-      </header>
-  );
+
+    const [state, setState] = useState({
+        theme: 'dark'
+    })
+    const switchTheme = () => {
+        state.theme === 'dark' ?
+        setState({theme: 'light'}) : setState({theme: 'dark'})
+    }
+
+    return (
+        <header className={`header ${state.theme}`}>
+            <Logo/>
+            <Menu/>
+            <div onClick={() => switchTheme()}>Change Theme</div>
+        </header>
+    );
 }
 
 export default App;
