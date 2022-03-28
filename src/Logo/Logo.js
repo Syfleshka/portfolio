9b1b8d728a48f30941515252fc8803c1c6b7484f
Syfle <syfle.denisov@gmail.com>
2022-03-28 18:05:28 +0300

    Logo styling

--- src/Logo/Logo.js
+++ src/Logo/Logo.js
@@ -2,9 +2,15 @@ import logo from './img/logo.png'
 import './Logo.scss'
 
 function Logo() {
+
     return (
-        <div className="header__logo">
-            <img className="logo" src={logo} alt="Alex D."/>
+        <div className="header__logo" id="headerLogo">
+            <img
+                id="logo"
+                className="logo"
+                src={logo}
+                alt="Alex D."
+            />
         </div>
     );
 }
