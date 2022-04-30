75a776da931c162b1d4fbfe36f6617885ae4473c
Syfle <syfle.denisov@gmail.com>
2022-04-30 20:14:14 +0300

    made transition simpler

--- src/Header/Menu/MenuItem.js
+++ src/Header/Menu/MenuItem.js
@@ -1,17 +1,10 @@
 import './MenuItem.scss';
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
 
 function MenuItem({ item }) {
-    const [wrapperPositionCorrector, setWrapperPositionCorrector] = useState(false);
     return (
-        <div className={`header-menu__wrapper${wrapperPositionCorrector ? ' header-menu__wrapper_anim' : ''}`}>
-            <NavLink
-                className="menu-item"
-                to={item.link}
-                onMouseOver={() => setWrapperPositionCorrector(true)}
-                onMouseOut={() => setWrapperPositionCorrector(false)}
-            >
+        <div className={`header-menu__wrapper`}>
+            <NavLink className="menu-item" to={item.link}>
                 <span className="menu-item__text">{item.label}</span>
                 <span className="menu-item__text">{item.label}</span>
                 <span className="menu-item__text">{item.label}</span>
