6038805088cd8088bd889e5b0f90e64dc7f49536
Syfle <syfle.denisov@gmail.com>
2022-05-06 15:35:17 +0300

    rename path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.scss';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from './Header/Header';
-import { Footer } from './Footer/Footer';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
     const [state, setState] = useState({
