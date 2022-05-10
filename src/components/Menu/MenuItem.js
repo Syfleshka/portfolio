58ab458e57e01447c8406de84bf1f155d207e15a
Syfle <syfle.denisov@gmail.com>
2022-05-10 15:43:32 +0300

    items map

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,22 +1,32 @@
 import './MenuItem.scss';
 import { NavLink } from 'react-router-dom';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
-function MenuItem({ item, id }) {
+function createMenuItemsArr(num, label) {
+    return [
+        Array.from({ length: num }).map((el, i) => (
+            <span className="menu-item__text" key={i}>
+                {label}
+            </span>
+        )),
+    ];
+}
+
+function MenuItem({ item: { label, link }, id }) {
     useEffect(() => {
         const menu = document.getElementById(`header-menu`);
-        const menuRect = menu.getBoundingClientRect()
+        const menuRect = menu.getBoundingClientRect();
         const menuItem = document.getElementById(`menu-item-effect-${id}`);
 
         const followMouse = (event) => {
-            const menuWidth = menuRect.width
-            const mousePosition = event.pageX - menuRect.left
-            const calcPercent = mousePosition / (menuWidth / 100)
-            menuItem.style.setProperty('left', `${calcPercent - 50}%`)
-        }
+            const menuWidth = menuRect.width;
+            const mousePosition = event.pageX - menuRect.left;
+            const calcPercent = mousePosition / (menuWidth / 100);
+            menuItem.style.setProperty('left', `${calcPercent - 50}%`);
+        };
         const mouseOut = () => {
-            menuItem.style.removeProperty('left')
-        }
+            menuItem.style.removeProperty('left');
+        };
 
         menuItem.addEventListener('mousemove', followMouse);
         menuItem.addEventListener('mouseleave', mouseOut);
@@ -25,29 +35,13 @@ function MenuItem({ item, id }) {
             menuItem.removeEventListener('mousemove', followMouse);
             menuItem.removeEventListener('mouseleave', mouseOut);
         };
-        
     }, [id]);
-
 
     return (
         <div className={`header-menu__wrapper`}>
             <div className={`menu-item-effect`} id={`menu-item-effect-${id}`}>
-                <NavLink className='menu-item' to={item.link}>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
-                    <span className='menu-item__text'>{item.label}</span>
+                <NavLink className="menu-item" to={link}>
+                    {createMenuItemsArr(15, label)}
                 </NavLink>
             </div>
         </div>
